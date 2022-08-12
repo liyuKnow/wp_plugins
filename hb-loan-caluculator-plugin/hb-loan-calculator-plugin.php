a828d5f053e57ac2b7f18e6a6a0a76a9ea200e94
liyuKnow <kidus2011@gmail.com>
2022-08-12 14:42:39 -0700

    fiished functionality and setup final plugin

---
+++ hb-loan-caluculator-plugin/hb-loan-calculator-plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: hb-loan-calculator-plugin
+ * Description: A sample wordpress plugin that calculates loan's monthly payment, interest and total principal added to interest.
+ * Version: 0.1
+ * Author: kidus-taye
+ * Author URI: https://www.liyudev.com/
+ **/
--- hb-loan-caluculator-plugin/js/script.js
+++ hb-loan-caluculator-plugin/js/script.js
@@ -1,31 +1,57 @@
-let loan_amount = document.querySelector('#loan_amount');
-let interest_rate = document.querySelector('#interest_rate');
-let loan_term = document.querySelector('#loan_term');
+// Get Inputs
+let loan_amount = document.querySelector('#loan-amount');
+let interest_rate = document.querySelector('#interest-rate');
+let loan_term = document.querySelector('#loan-term');
 
+// On load calculate the monthly payment
+calculateMonthlyPayment();
+
+// set on keyup event listener to loan amount
+loan_amount.addEventListener('keyup', calculateMonthlyPayment);
 
 // Calculate the monthly payment
 function calculateMonthlyPayment() {
-    let principal = parseFloat(loan_amount.value);
-    let calculatedInterestRate = parseFloat(interest_rate.value) / 100 / 12;
-    let calculatedLoanTerm = parseFloat(loan_term.value) * 12;
-
+    console.log('calculateMonthlyPayment');
+    // get the values from the inputs
+    let principal = parseFloat(loan_amount.value) ? parseFloat(loan_amount.value) : 500000;
+    let calculatedInterestRate = parseFloat(interest_rate.value)? ( parseFloat(interest_rate.value) / 100 ) : (15 / 100) ;
+    let calculatedLoanTerm = parseFloat(loan_term.value)?  parseFloat(loan_term.value) * 12 : 60 * 12;
+    
+    console.log({principal, calculatedInterestRate, calculatedLoanTerm});
     let x = Math.pow(1 + calculatedInterestRate, calculatedLoanTerm);
     let monthly = (principal*x*calculatedInterestRate)/(x-1);
+
     // Calculate the total payment
     let total = monthly * calculatedLoanTerm;
-    if (isFinite(total)) {
-        document.querySelector('#total_payment').value = total.toFixed(2);
+    // if (isFinite(total)) {
+    //     document.querySelector('#monthly_payment').innerHTML = total.toFixed(2);
+    // } else {
+    //     document.querySelector('#monthly_payment').innerHTML = '';
+    // }
+
+    // calculate monthly payment
+    let monthlyPayment = principal * (calculatedInterestRate / 12);
+    if (isFinite(monthlyPayment)) {
+        // set number to 2 decimal places and separate with commas
+
+        document.querySelector('#monthly_payment').innerHTML = monthlyPayment.toLocaleString('en-US', {maximumFractionDigits: 2});
     } else {
-        document.querySelector('#total_payment').value = '';
+        document.querySelector('#monthly_payment').innerHTML = '';
     }
 
     // Calculate the total interest
-    let totalInterest = total - principal;
+    let totalInterest = principal * calculatedInterestRate;
     if (isFinite(totalInterest)) {
-        document.querySelector('#total_interest').value = totalInterest.toFixed(2);
+        document.querySelector('#total-interest').innerHTML = totalInterest.toLocaleString('en-US', {maximumFractionDigits: 2});
     } else {
-        document.querySelector('#total_interest').value = '';
+        document.querySelector('#total-interest').innerHTML = '';
     }
 
-
+    // Total principal interest
+    let totalPrincipalInterest = total + totalInterest;
+    if (isFinite(totalPrincipalInterest)) {
+        document.querySelector('#total-principal-interest').innerHTML = totalPrincipalInterest.toLocaleString('en-US', {maximumFractionDigits: 2});
+    } else {
+        document.querySelector('#total-principal-interest').innerHTML = '';
+    }
 }
